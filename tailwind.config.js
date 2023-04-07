dd2adc611e79d0dcb4570cec87940f3307027aa9
Camilo Gaitan <camilo.gaitan09@gmail.com>
2023-04-07 17:31:29 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*{html,js}"],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
