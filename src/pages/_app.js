c6097e162b4e34ead40e11332df810138cf8519a
Camilo Gaitan <camilo.gaitan09@gmail.com>
2023-04-07 20:02:51 -0500

    Actualización 100% funcional hasta la clase 10

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "../styles/tailwind.css";
+import MainLayout from "@layout/MainLayout";
+import "@styles/tailwind.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
+
+export default MyApp;
